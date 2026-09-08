9e27d1a337929ca1071ab44524aa783353d64b94
Neburionxv73 <michaelseb96@gmail.com>
2026-09-08 13:09:55 +0200

    feat(i18n): wrap BrainFit with English content bridge

--- Neburion_XV73_KogTrain_V6.5_Platform/components/BrainFitClient.tsx
+++ Neburion_XV73_KogTrain_V6.5_Platform/components/BrainFitClient.tsx
@@ -1,34 +1,13 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { BrainFitEnglishBridge } from "./BrainFitEnglishBridge";
 
-const BrainFitTraining = dynamic(
-  () => import("./BrainFitTraining").then((module) => module.BrainFitTraining),
-  { ssr: false },
-);
-
-const BrainFitAdaptiveV5 = dynamic(
-  () => import("./BrainFitAdaptiveV5").then((module) => module.BrainFitAdaptiveV5),
-  { ssr: false },
-);
-
-const BrainFitCompletionPanel = dynamic(
-  () => import("./BrainFitCompletionPanel").then((module) => module.BrainFitCompletionPanel),
-  { ssr: false },
-);
-
-const UnifiedTrainingCoach = dynamic(
-  () => import("./UnifiedTrainingCoach").then((module) => module.UnifiedTrainingCoach),
-  { ssr: false },
-);
+const BrainFitTraining = dynamic(() => import("./BrainFitTraining").then((module) => module.BrainFitTraining), { ssr: false });
+const BrainFitAdaptiveV5 = dynamic(() => import("./BrainFitAdaptiveV5").then((module) => module.BrainFitAdaptiveV5), { ssr: false });
+const BrainFitCompletionPanel = dynamic(() => import("./BrainFitCompletionPanel").then((module) => module.BrainFitCompletionPanel), { ssr: false });
+const UnifiedTrainingCoach = dynamic(() => import("./UnifiedTrainingCoach").then((module) => module.UnifiedTrainingCoach), { ssr: false });
 
 export function BrainFitClient() {
-  return (
-    <>
-      <BrainFitTraining />
-      <BrainFitAdaptiveV5 />
-      <BrainFitCompletionPanel />
-      <UnifiedTrainingCoach />
-    </>
-  );
+  return <BrainFitEnglishBridge><BrainFitTraining /><BrainFitAdaptiveV5 /><BrainFitCompletionPanel /><UnifiedTrainingCoach /></BrainFitEnglishBridge>;
 }
