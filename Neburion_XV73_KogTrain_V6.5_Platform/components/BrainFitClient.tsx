b66c033f643c3d9fefa9bbbbf7626396a8dc0659
Neburionxv73 <michaelseb96@gmail.com>
2026-09-08 13:45:14 +0200

    feat(i18n): switch BrainFit datasets with platform language

--- Neburion_XV73_KogTrain_V6.5_Platform/components/BrainFitClient.tsx
+++ Neburion_XV73_KogTrain_V6.5_Platform/components/BrainFitClient.tsx
@@ -3,6 +3,7 @@
 import dynamic from "next/dynamic";
 import { BrainFitEnglishBridge } from "./BrainFitEnglishBridge";
 import { usePlatformLanguage } from "./PlatformLanguageProvider";
+import { applyBrainFitLanguageData } from "@/lib/brainFitLocaleData";
 
 const BrainFitTraining = dynamic(() => import("./BrainFitTraining").then((module) => module.BrainFitTraining), { ssr: false });
 const BrainFitAdaptiveV5 = dynamic(() => import("./BrainFitAdaptiveV5").then((module) => module.BrainFitAdaptiveV5), { ssr: false });
@@ -11,5 +12,6 @@ const UnifiedTrainingCoach = dynamic(() => import("./UnifiedTrainingCoach").then
 
 export function BrainFitClient() {
   const { language } = usePlatformLanguage();
-  return <BrainFitEnglishBridge key={language}><BrainFitTraining /><BrainFitAdaptiveV5 /><BrainFitCompletionPanel /><UnifiedTrainingCoach /></BrainFitEnglishBridge>;
+  applyBrainFitLanguageData(language);
+  return <BrainFitEnglishBridge key={language}><BrainFitTraining key={`brainfit-training-${language}`} /><BrainFitAdaptiveV5 key={`brainfit-adaptive-${language}`} /><BrainFitCompletionPanel /><UnifiedTrainingCoach /></BrainFitEnglishBridge>;
 }
